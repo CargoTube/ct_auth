26ce290281de990ec94d5ed327ec052836d8f2c0
bwegh <baswegh@gmx.de>
2018-05-13 16:23:49 +0200

    only return info, do not active send messages

--- src/ct_auth.erl
+++ src/ct_auth.erl
@@ -10,12 +10,11 @@
 handle_hello({hello, RealmName, Details}, Peer) ->
     Result = cta_realm:lookup(RealmName),
     SessionResult = handle_realm(Result, Details, Peer),
-    send_welcome_challenge_or_abort(SessionResult, Peer).
+    return_welcome_challenge_or_abort(SessionResult).
 
 
-handle_authenticate(_Authenticate, PeerAtGate) ->
-    ct_router:to_peer(PeerAtGate ,{to_peer, ?ABORT(#{}, canceled)}),
-    ok.
+handle_authenticate(_Authenticate, _PeerAtGate) ->
+    {abort, canceled}.
 
 is_message_allowed(_Message, _Session) ->
     %% TODO: implement
@@ -68,38 +67,29 @@ get_client_authmethods(Details, _) ->
     maps:get(<<"authmethods">>, Details, []).
 
 
-send_welcome_challenge_or_abort({ok, Session, anonymous, Realm}, _Peer) ->
+return_welcome_challenge_or_abort({ok, Session, anonymous, Realm}) ->
     {ok, NewSession} = cta_session:set_auth_details(anonymous, anonymous,
                                                     anonymous, Session),
     RoleResult = cta_realm:get_role(anonymous, Realm),
     maybe_authenticate_session(RoleResult, NewSession);
-send_welcome_challenge_or_abort({error, no_such_realm}, PeerAtGate) ->
-    send_abort(PeerAtGate, no_such_realm);
-send_welcome_challenge_or_abort({error, no_such_auth_method}, PeerAtGate) ->
-    send_abort(PeerAtGate, invalid_argument);
-send_welcome_challenge_or_abort({ok, Session, _, _}, _PeerAtGate) ->
+return_welcome_challenge_or_abort({error, no_such_realm}) ->
+    return_abort(no_such_realm);
+return_welcome_challenge_or_abort({error, no_such_auth_method}) ->
+    return_abort(invalid_argument);
+return_welcome_challenge_or_abort({ok, Session, _, _}) ->
     abort_session(Session);
-send_welcome_challenge_or_abort( _, PeerAtGate) ->
-    send_abort(PeerAtGate, canceled).
+return_welcome_challenge_or_abort( _) ->
+    return_abort(canceled).
 
 maybe_authenticate_session({ok, Role}, Session) ->
-    {ok, NewSession} = cta_session:authenticate(Role, Session),
-    SessionId = cta_session:get_id(NewSession),
-    Details = #{
-      agent => ct_router:agent_identification(),
-      roles => ct_router:agent_roles()
-     },
-    ct_router:to_session(Session,?WELCOME( SessionId, Details)),
-    ok;
+    cta_session:authenticate(Role, Session);
 maybe_authenticate_session(_, Session) ->
     abort_session(Session).
 
 
 abort_session(Session) ->
-    Peer = cta_session:get_peer(Session),
     ok = cta_session:close(Session),
-    send_abort(Peer, canceled).
+    return_abort(canceled).
 
-send_abort(Peer, Reason) ->
-    ct_router:to_peer(Peer, {to_peer, ?ABORT(#{}, Reason)}),
-    ok.
+return_abort(Reason) ->
+    {abort, Reason}.
